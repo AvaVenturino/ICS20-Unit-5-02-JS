1442d7f2d80decd0c72e4f0f14a9fb07d98e8db5
Ava Venturino <99355031+AvaVenturino@users.noreply.github.com>
2022-04-26 17:05:59 +0000

    edited files

--- js/script.js
+++ js/script.js
@@ -8,6 +8,7 @@
  * Check servie worker.
  */
 if (navigator.serviceWorker) {
+<<<<<<< HEAD
   navigator.serviceWorker.register("/ICS20-Unit-5-02-JS/sw.js", {
     scope: "/ICS20-Unit-5-02-JS/",
   })
@@ -23,13 +24,45 @@ if (navigator.serviceWorker) {
   function myButtonClicked() {
   const positive = prompt("Enter the number of students: ")
   const negative = prompt("Enter the number of students: ")
+=======
+  navigator.serviceWorker.register("/ICS20-Unit-5-01-JS/sw.js", {
+    scope: "/ICS20-Unit-5-01-JS/",
+  });
+}
+
+const randomNumber = Math.floor(Math.random() * 6) + 1;
+
+/**
+ * This function updates the slider value.
+ */
+function updateSliderValue(valueFromSlider) {
+  document.getElementById("slider-value").innerHTML = valueFromSlider;
+}
+
+/**
+ * This function compares slider with random number.
+ */
+function myButtonClicked() {
+  const sliderValue = slider.value;
+>>>>>>> origin/main
 
   // process
   if (numbervalue < 0) {
     document.getElementById("answer").innerHTML =
+<<<<<<< HEAD
       "The number is " + Negative + "!"
   } else (sliderValue > 0) {
     document.getElementById("answer").innerHTML =
       "The number is " + Positive + "!"
+=======
+      "Answer is " + randomNumber + "!" + " Congratulations!";
+    // console.log("Congratulations!")
+  }
+  //  block of code to be executed if condition1 is true
+  if (sliderValue != randomNumber) {
+    document.getElementById("answer").innerHTML =
+      "Answer is " + randomNumber + "!" + " Try again!";
+    // console.log("Try again!")
+>>>>>>> origin/main
   }
 }
