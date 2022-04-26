8bdf28708c48b9a479f2d16074e1b52aad34f0cb
AvaVenturino <AvaVenturino@users.noreply.github.com>
2022-04-26 16:16:06 +0000

    Prettified Code!

--- js/script.js
+++ js/script.js
@@ -10,34 +10,34 @@
 if (navigator.serviceWorker) {
   navigator.serviceWorker.register("/ICS20-Unit-5-01-JS/sw.js", {
     scope: "/ICS20-Unit-5-01-JS/",
-  })
+  });
 }
 
-const randomNumber = Math.floor(Math.random() * 6) + 1
+const randomNumber = Math.floor(Math.random() * 6) + 1;
 
 /**
  * This function updates the slider value.
  */
 function updateSliderValue(valueFromSlider) {
-  document.getElementById("slider-value").innerHTML = valueFromSlider
+  document.getElementById("slider-value").innerHTML = valueFromSlider;
 }
 
 /**
  * This function compares slider with random number.
  */
 function myButtonClicked() {
-  const sliderValue = slider.value
+  const sliderValue = slider.value;
 
   // process
   if (sliderValue == randomNumber) {
     document.getElementById("answer").innerHTML =
-      "Answer is " + randomNumber + "!" + " Congratulations!"
+      "Answer is " + randomNumber + "!" + " Congratulations!";
     // console.log("Congratulations!")
   }
   //  block of code to be executed if condition1 is true
   if (sliderValue != randomNumber) {
     document.getElementById("answer").innerHTML =
-      "Answer is " + randomNumber + "!" + " Try again!"
+      "Answer is " + randomNumber + "!" + " Try again!";
     // console.log("Try again!")
   }
 }
